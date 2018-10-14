53adfc92ef181a4c4831642ce291b86316764fbd
Diego Armando <Edu.Diegoarmando@hotmail.com>
2018-10-14 17:51:10 -0300

    created project - metadata

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
-var express = require("express");
-var app = express();
+const express = require("express");
+const app = express();
 
 app.use(express.static(__dirname + "/public"));
 
-app.listen(3000, function() {
+app.listen(3000, () => {
   console.log("Conectou-se na porta 3000!");
 });
 
