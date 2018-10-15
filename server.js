055bac0e57d5e8c2073b93d8240dff4b4e74e4d2
Diego Armando <Edu.Diegoarmando@hotmail.com>
2018-10-14 21:04:49 -0300

    forms and desing created

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const app = express();
+var multer = require("multer");
+var upload = multer({});
 
 app.use(express.static(__dirname + "/public"));
 
@@ -8,3 +10,12 @@ app.listen(3000, () => {
 });
 
 app.get("/", (req, res) => res.sendFile("index.html"));
+
+app.post("/fileanalyse", upload.single("upfile"), (req, res, next) => {
+  const archive = req.file;
+  res.json({
+    name: archive.originalname,
+    type: archive.mimetype,
+    size: archive.size
+  });
+});
